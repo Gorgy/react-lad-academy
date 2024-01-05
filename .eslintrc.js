8d8362903d41a0787c604e1ffc205bb0e3c6193d
Belov Alexey <belovaa@lad24.ru>
2024-01-05 22:12:24 +0300

    feat: add stylelint and commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    sourceType: 'module',
   },
   settings: {
     'import/resolver': {
